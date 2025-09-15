f8e6d51677b28bbc907553f143416ff4db5155a5
angelomimmo04 <a.mimmo@studenti.poliba.it>
2025-09-15 20:47:40 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ const app = express();
 // --- Middleware ---
 const allowedOrigins = [
     "http://localhost:5173",
-    "https://be-poli-pxil.onrender.com/",
+    "https://be-poli-pxil.onrender.com",
     "http://localhost:3000",
     "https://bepoli.onrender.com"
 ];
